f0a9684d9db11cfc17d3c2db543008f2d97401bf
Pablo Rebaque Diaz <pablo.rebaque.diaz@gmail.com>
2018-09-29 17:27:53 +0200

    Creating project in eclipse IDE

---
+++ brainlord/src/brainlord.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : brainlord.cpp
+// Author      : Pablo Rebaque Díaz
+// Version     :
+// Copyright   : Free to copy
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
