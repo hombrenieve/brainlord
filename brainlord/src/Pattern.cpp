e1e72a460c234619986111a82822b035baf26de7
Pablo Rebaque Diaz <pablo.rebaque.diaz@gmail.com>
2018-09-30 08:26:33 +0200

    Pattern engineered

--- brainlord/src/Pattern.cpp
+++ brainlord/src/Pattern.cpp
@@ -6,18 +6,50 @@
  */
 
 #include "Pattern.h"
+#include <random>
+#include <algorithm>
+
+const char Pattern::validChars[]  = { 'R', 'G', 'B', 'W', 'Y', 'P' };
 
 Pattern::Pattern() {
-	// TODO Auto-generated constructor stub
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<int> dist(0, sizeof(validChars)-1);
 
+	for(int i = 0; i < SIZE; i++) {
+		pattern.push_back(validChars[dist(mt)]);
+	}
+}
+
+Pattern::Pattern(const std::vector<int> pattern) :
+	pattern(pattern) {
+	//TODO: check preconditions
+}
+
+Pattern::Pattern(const char* pattern) {
+	//TODO: check preconditions
+	for(int i = 0; i < SIZE; i++) {
+		this->pattern.push_back(pattern[i]);
+	}
+
+}
+
+FeedBack Pattern::check(Pattern guess) {
+	return FeedBack();
 }
 
 Pattern::~Pattern() {
 	// TODO Auto-generated destructor stub
 }
 
-FeedBack Pattern::check(Pattern guess) {
-	return FeedBack();
+bool Pattern::isValid(char character) {
+	for(int i=0; i < sizeof(validChars); i++) {
+		if(validChars[i] == character) {
+			return true;
+		}
+	}
+	return false;
 }
+
 
 
--- brainlord/src/Pattern.h
+++ brainlord/src/Pattern.h
@@ -8,12 +8,20 @@
 #ifndef PATTERN_H_
 #define PATTERN_H_
 #include "FeedBack.h"
+#include <vector>
 
 class Pattern {
+	static const char validChars[];
 public:
+	static const int SIZE = 4;
 	Pattern();
+	Pattern(const std::vector<int> pattern);
+	Pattern(const char* pattern);
 	virtual ~Pattern();
 	FeedBack check(Pattern guess);
+private:
+	std::vector<int> pattern;
+	bool isValid(char character);
 };
 
 #endif /* PATTERN_H_ */
